0d8887f137e34d1d4ca9075075d8136624676618
Fiorela <clafiorelariza@gmail.com>
2024-06-13 21:30:07 -0500

    creando directorio de proyecto y aplicacion

--- miPRoyecto/app1/models.py
+++ miPRoyecto/app1/models.py
@@ -5,4 +5,12 @@ class Persona (models.Model):
     nombre = models.CharField(max_length=20)
     edad = models.IntegerField(null=True, blank=True)
     def __str__(self):
+        
+        return self.nombre
+    
+class Cliente (models.Model):
+    nombreCli = models.CharField(max_length=20)
+    edad = models.IntegerField(null=True, blank=True)
+    def __str__(self):
+        
         return self.nombre
